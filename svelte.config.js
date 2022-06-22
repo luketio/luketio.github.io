76a38e48df45cb5ac17ca192182162cce103e4d5
ltgr <luke.rtong@gmail.com>
2022-06-22 18:46:50 -0400

    added mdsvex for blog posts

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
@@ -13,6 +17,7 @@ const config = {
 
 			postcss: true,
 		}),
+		mdsvex(mdsvexConfig),
 	],
 
 	kit: {
